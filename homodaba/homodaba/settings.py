ade916b95f357622cb356415e783e4f77a78cb05
moz667 <moz667@gmail.com>
2021-07-03 10:43:23 +0200

    upsie2, meter por defecto 127.0.0.1 en LOCALNETIP.

--- homodaba/homodaba/settings.py
+++ homodaba/homodaba/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-LOCALNETIP = os.getenv("LOCALNETIP")
+LOCALNETIP = os.getenv("LOCALNETIP", '127.0.0.1')
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', LOCALNETIP).split()
 
 # Application definition
