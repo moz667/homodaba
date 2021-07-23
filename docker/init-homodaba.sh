bca62f3c8500310b9955f158e18e813d97081826
moz667 <moz667@gmail.com>
2021-07-23 08:29:08 +0200

    Apañando el tema de sqlite3 path y poniendo un shortcut a manage

--- docker/init-homodaba.sh
+++ docker/init-homodaba.sh
@@ -4,7 +4,5 @@ echo " * homodaba:trunk init *"
 
 cd /opt/app/homodaba/homodaba
 
-export SQLITE3_PATH="/opt/app/sqlite3"
-
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
---
+++ docker/manage.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /opt/app/homodaba/homodaba 
+
+python manage.py $*
