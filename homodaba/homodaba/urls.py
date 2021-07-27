02438c97b365cc7af6683bb80aa4f8e3b77d0923
moz667 <moz667@gmail.com>
2021-07-27 18:55:25 +0200

    Fallo urls dockerizadas y quitando chocho-variables de composer

--- homodaba/homodaba/urls.py
+++ homodaba/homodaba/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import include, path
 from .settings import HOME_URL_PATH
 
 urlpatterns = [
     path('%sadmin/' % HOME_URL_PATH, admin.site.urls),
+    path('%sauth/' % HOME_URL_PATH, include('django.contrib.auth.urls')),
+    path('%si18n/' % HOME_URL_PATH, include('django.conf.urls.i18n')),
 ]
