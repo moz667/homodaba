f22bcab8aac53de4fb038b144f0daaa9442aecde
moz667 <moz667@gmail.com>
2021-08-13 11:37:34 +0200

    un script un poco mas elaborado para construir los zip

--- kodi/build.sh
+++ kodi/build.sh
@@ -1,15 +1,33 @@
+build_addon_zip() {
+	ADDON_NAME=$1
+	KODI_VERSION=$2
+	OUTPUT_ZIP_FILE="$ADDON_NAME-$KODI_VERSION.zip"
+
+	echo " * Construyendo $ADDON_NAME ($KODI_VERSION) en $OUTPUT_ZIP_FILE"
+
+	if [ -f $OUTPUT_ZIP_FILE ]; then
+		rm $OUTPUT_ZIP_FILE
+	fi
+
+	zip -qr $OUTPUT_ZIP_FILE $ADDON_NAME/
+}
+
 mkdir -p build
 
-rsync -va plugin.homodaba.movies/ build/plugin.homodaba.movies/
+rsync -qa --del plugin.homodaba.movies/ build/plugin.homodaba.movies/
+
+ADDON_DESC_FILE=plugin.homodaba.movies/addon.xml
 
 cd build
 
-if [ -f plugin.homodaba.movies.zip ]; then
-	rm plugin.homodaba.movies.zip
-fi
-
-if [-e settings.xml]; then
+if [ -e settings.xml ]; then
 	cp settings.xml plugin.homodaba.movies/resources/
 fi
 
-zip -r plugin.homodaba.movies.zip plugin.homodaba.movies/
+build_addon_zip plugin.homodaba.movies matrix
+
+
+ADDON_DESC_FILE=plugin.homodaba.movies/addon.xml
+sed -i 's/addon="xbmc.python" version="3.0.0"/addon="xbmc.python" version="2.25.0"/g' $ADDON_DESC_FILE
+
+build_addon_zip plugin.homodaba.movies leia
