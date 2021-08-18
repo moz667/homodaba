90ca85e88b482866840a0ab4128196dc057451a9
bpk667 <pedro.pozuelo@gmail.com>
2021-08-18 10:03:42 +0200

    Feature. Django waits for MySQL before running.
    Minor Fixes.
      MySQL wasn't created properly if missing.
      MySQL root user is not mandatory. Default user is homodaba_user

--- docker/app/init-homodaba.sh
+++ docker/app/init-homodaba.sh
@@ -4,5 +4,13 @@ echo " * homodaba:trunk init *"
 
 cd /opt/app/homodaba
 
+# Checking SQL server is ready.
+if [ "$DATABASE_ENGINE" == "mysql" ] ; then
+  while ! mysqladmin -u"$DATABASE_USER" -p"$DATABASE_PASSWORD" -h"$DATABASE_HOST" ping --silent ; do
+    echo 'Waiting for SQL to be ready'
+    sleep 1
+  done
+fi
+
 python manage.py migrate
 exec python manage.py runserver 0.0.0.0:8000
