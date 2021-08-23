e69fd5a6aa345c868bb6969f0ebfbe79a77432fb
moz667 <moz667@gmail.com>
2021-08-23 08:00:05 +0200

    buscando contenedores con regex .*

--- manage.sh
+++ manage.sh
@@ -8,6 +8,12 @@ if [ -z "$CONTAINER_NAME" ]; then
     CONTAINER_NAME=`docker ps -a|grep homodaba-app|grep "Up "|sed -e "s/.* //g"`
 fi
 
+# Si todavia no hemos encontrado ninguno... probamos con regexp 
+# es posible que tengamos alguno con otro sobrenombre
+if [ -z "$CONTAINER_NAME" ]; then
+    CONTAINER_NAME=`docker ps -a|grep "homodaba.*-app"|grep "Up "|sed -e "s/.* //g"`
+fi
+
 if [ -z "$CONTAINER_NAME" ]; then
     echo "No encuentro el contenedor de homodaba-app arrancado..."
     echo "Puedes arrancarlo de forma sencilla a traves de compose con:"
