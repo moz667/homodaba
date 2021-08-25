8ff4edc9afe28e87734d3f605beab2b829f189ae
bpk667 <pedro.pozuelo@gmail.com>
2021-08-25 11:31:33 +0200

    Added healthchecks.

--- docker/app/init-homodaba.sh
+++ docker/app/init-homodaba.sh
@@ -1,24 +1,19 @@
-#!/bin/sh
+#!/bin/sh -e
 
 echo " * homodaba:trunk init *"
 
 cd /opt/app/homodaba
 
-# Checking SQL server is ready.
+# Checking if MySQL DB is ready.
 if [ "$DATABASE_ENGINE" = "mysql" ] ; then
-  while true; do
-    echo 'Waiting for SQL to be ready'
-    if [ -z "$DATABASE_PASSWORD" ]; then
-      mysqladmin -u"$DATABASE_USER" -h"$DATABASE_HOST" ping --silent
-    else
-      mysqladmin -u"$DATABASE_USER" -p"$DATABASE_PASSWORD" -h"$DATABASE_HOST" ping --silent
-    fi
-    
-    if [ $? = 0 ]; then
-      break
-    fi
-    sleep 1
-  done
+  if [ -n "$DATABASE_PASSWORD" ]; then
+    PWD_PARAM="-p${DATABASE_PASSWORD}"
+  fi
+  until \
+    echo 'Waiting for SQL to be ready' && \
+    mysqladmin -u"$DATABASE_USER" "${PWD_PARAM}" -h"$DATABASE_HOST" ping --silent; do
+      sleep 1
+    done
 fi
 
 python manage.py migrate
