5518156c6a03ea2c4f6974d36bcdbafcd30f56e0
moz667 <moz667@gmail.com>
2021-08-28 09:52:41 +0200

    @bpk667, esta listo mas o menos a falta de alguna cosilla que podemos comentar.

--- start.sh
+++ start.sh
@@ -1,6 +1,21 @@
 #!/bin/bash -e
 
-ENVFILE='.env'
+# TODO: Chequear dependencias...
+# Requisitos:
+#   - docker
+#   - docker-compose
+#   - python3
+
+ENVFILE=".env"
+# TODO: Revisar... Esto no me acaba de convencer... para no mostrar el 
+# WARNING de la contraseña de mysql solo he visto esta forma... Lo cual
+# nos desactiva todos los mensajes que no sean ERROR del compose, por lo
+# que tendremos que contar que esta haciendo en cada momento...
+COMPOSE="docker-compose --log-level ERROR"
+# manage.py interactive (attach tty)
+MANAGE="$COMPOSE exec app python homodaba/manage.py"
+# manage.py shell -c NOT INTERACTIVE (Disable pseudo-tty allocation)
+MANAGE_SHELL="$COMPOSE exec -T app python homodaba/manage.py shell -c"
 
 generate_secret() {
     size=$1
@@ -8,53 +23,110 @@ generate_secret() {
 }
 
 wait_until_healthy() {
-	service="app"
+	service="$1"
 	
-    # FIXME: Ojo, esto es lo que fallaba el otro dia, si hay varios contenedores
-    # que se asemejen (app es demasiado comun...) entonces container_id seria un MAP
-    container_id="$(docker-compose ps -q "$service")"
-    health_status="$(docker inspect -f "{{.State.Health.Status}}" "$container_id")"
+    container_id="$($COMPOSE ps -q "$service")"
 
-    while [[ "$health_status" != "healthy" ]] ; do 
-        echo 'Waiting for application to be ready'
+    while true ; do 
         health_status="$(docker inspect -f "{{.State.Health.Status}}" "$container_id")"
+        if [ "$health_status" == "healthy" ]; then
+            echo "  - $service is ready"
+            break
+        fi
+        echo "      * Waiting for $service to be ready..."
         sleep 5
     done
 }
 
-# FIXME: Ojo, acabo de ver que docker-compose usa por defecto .env... esto hace
-# que toda esta mierda sea innecesaria
-COMPOSE_ARGS="--env-file $ENVFILE"
+wait_until_finish() {
+	service="$1"
+	
+    container_id="$($COMPOSE ps -q "$service")"
+
+    while true ; do 
+        status="$(docker inspect -f "{{.State.Status}}" "$container_id")"
+        if [ "$status" == "exited" ]; then
+            echo "  - $service is finished"
+            break
+        fi
+        echo "      * Waiting for $service to be finished..."
+        sleep 5
+    done
+}
 
 if [ ! -f $ENVFILE ]; then
-    SECRET_KEY=generate_secret(37)
-    COMPOSE_ARGS="-e SECRET_KEY"
+    SECRET_KEY=`python3 -c "import secrets;print(secrets.token_urlsafe(37))"`
+    echo "SECRET_KEY=$SECRET_KEY" > $ENVFILE
 fi
 
-docker-compose $COMPOSE_ARGS up -d --build
+# TODO: Borrar todo este comentario una vez lo hable con @bpk667
+# Esto es un comportamiento curiosos de compose...
+# Cuando extiendes de otro compose, no detecta cambios
+# en el Dockerfile del padre, por ello, para asegurarnos
+# que detecta cambios en ello tendriamos que hacer algo asi:
+# echo "Building app..."
+# $COMPOSE -f docker-compose.base.yml build "app"
+# Esta comentado ya que queremos mantenerlo lo mas simple posible :P
+
+echo "Starting homodaba..."
+
+# TODO: Esta comprobacion chusca. La he puesto para que evite arrancar static-build
+# cada vez que se ejecute 
+if [ ! -f static/build/css/main.thirdparty.css ]; then
+    echo "  - building statics..."
+    $COMPOSE up -d "static-build"
+    wait_until_finish "static-build"
+fi
+
+echo "  - starting app..."
+$COMPOSE up -d "app"
 
 # 1) Esperar hasta que termine de arrancar el tema
-wait_until_healthy
+wait_until_healthy "app"
 
 # 2) Si no existe superuser en la bbdd pedimos para crearlo
-check_superusers=$(cat <<EOF
-from django.contrib.auth import get_user_model
-print(len(get_user_model().objects.filter(is_superuser=True).all()))
-EOF
-)
-
-# FIXME: idem que con el tema del map... (ver mas arriba)
-django_manage="docker-compose exec -T app python homodaba/manage.py shell -c"
-
-superusers_count=`$django_manage $check_superusers`
+superusers_count=`$MANAGE_SHELL "\
+from django.contrib.auth import get_user_model;\
+print(len(get_user_model().objects.filter(is_superuser=True).all()));\
+"`
 
 if [ "$superusers_count" == "0" ]; then
-    echo 'No users. Created a new one:'
-    $django_manage createsuperuser
+    echo ""
+    echo "No superusers found. Do you want a new one? [Y/n]: "
+    read create_superuser
+    if [[ "$create_superuser" == "y" || "$create_superuser" == "Y" || "$create_superuser" == "" ]]; then
+        $MANAGE createsuperuser
+    fi
 fi
 
-# 3) TODO: Si no tiene datos damos opcion a importar demo-data ??? por ahora TODO
+# 3) Si no tiene datos damos opcion a importar demo-data
+movies_count=`$MANAGE_SHELL "\
+from data.models import Movie;\
+print(len(Movie.objects.all()));\
+"`
 
-# 4) Chequeamos:
-#   - Si no tiene volumes (import sqlite) lo notificamos, import no es importante
-#   - Si no tiene .env, notificamos SECRET_KEY
+if [ "$movies_count" == "0" ]; then
+    echo "No Movies. Do you want some sample data? [Y/n]: "
+    read create_movies
+    if [[ "$create_movies" == "y" || "$create_movies" == "Y" || "$create_movies" == "" ]]; then
+        # TODO: Crear un csv con datos de prueba
+        echo "Just kidding!! Its not implemented yet ^_^"
+    fi
+fi
+
+# 4) Mostramos el resultado de la instalacion:
+echo ""
+echo " **************************************************************************"
+echo " * Note that:                                                             *"
+echo " *   - This is a test version, the stored data will be destroyed if it    *"
+echo " *     remove the app container                                           *"
+echo " *   - The script could have created a secret key for you and save it on  *"
+echo " *     a .env file.                                                       *"
+echo " *     This key is very important for the user authentication             *"
+echo " *     If you delete and / or recreate it, this authentication will not   *"
+echo " *     work.                                                              *"
+echo " **************************************************************************"
+echo ""
+echo " - That's all!!!"
+echo " - Visit http://127.0.0.1:8000/ for some fun."
+echo ""
