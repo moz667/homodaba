489a8916afb2264cb4a7c0aa2fcf70e327fcb909
moz667 <moz667@gmail.com>
2021-12-30 06:21:12 +0100

    Adding gunicorn to real env deploy)

--- docker/app/init-homodaba.sh
+++ docker/app/init-homodaba.sh
@@ -23,4 +23,18 @@ if [ "$CACHE_DATABASE" != '0' ]; then
   python manage.py migrate --database cache
 fi
 
-exec python manage.py runserver 0.0.0.0:8000
+## FIXME: Problema con estaticos y gunicorn con DJANGO_DEBUG
+## Si queremos usar gunicorn con DJANGO_DEBUG, tenemos que buscar una forma 
+## alternativa de servir estaticos y aunque para eso montamos el proxy, no nos 
+## interesa desde el punto de vista de tener watch de todos los estaticos... por
+## eso por ahora vamos a dejarlo asin (si usas DJANGO_DEBUG, asumimos que no 
+## sirves estaticos de otra forma)
+if [ "$DJANGO_DEBUG" = "1" ] ; then
+  echo ""
+  echo "********************"
+  echo "*** DJANGO_DEBUG ***"
+  echo "********************"
+  exec python manage.py runserver 0.0.0.0:8000
+else
+  exec gunicorn -b :8000 -w 4 homodaba.wsgi:application
+fi
