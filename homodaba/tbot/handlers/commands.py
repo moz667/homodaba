3f42ec1cd1371d7aec946e33e5b1d960572b6cbb
moz667 <moz667@gmail.com>
2026-02-18 06:36:44 +0100

    feat(upgrade): Telegram fixes for upgrades (2)

--- homodaba/tbot/handlers/commands.py
+++ homodaba/tbot/handlers/commands.py
@@ -1,77 +1,45 @@
-import asyncio
+from homodaba.settings import TBOT_TOKEN
 
-from data.models import Movie
-from data.search import movie_search_filter
-
-from asgiref.sync import sync_to_async
-from homodaba.settings import TBOT_TOKEN, TBOT_LIMIT_MOVIES
-
-from .utils import print_movie, print_movies
+from .utils import get_movie_detail_html_by_id, search_movies_as_html
 
 import telegram
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ContextTypes
 
-help_message = """
-<b>/help: </b> Muestra este mensaje.
-<b>[/search] texto [(año)]: </b> Busca peliculas que coincidan con texto (opcionalmente del año entre parentesis). Si se especifica el texto entre comillas dobles, busca términos exactos.
-<b>/movie id: </b> Muestra el detalle de la película con ese id.
-"""
-
-def sync_print_reply_text(update: Update, message):
-    asyncio.run(update.message.reply_text(message))
-
-def sync_print_reply_html(update: Update, message):
-    asyncio.run(update.message.reply_html(message))
-
-
-@sync_to_async
-def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # """Send a message when the command /start is issued."""
-    bot_info = asyncio.run(telegram.Bot(TBOT_TOKEN).get_me())
-    sync_print_reply_text(update, f"Bienvenido a {bot_info.username}!")
-    sync_print_reply_html(update, help_message)
+    bot_info = await telegram.Bot(TBOT_TOKEN).get_me()
+    await update.message.reply_text(f"Bienvenido a {bot_info.username}!")
+    await help_command(update, context)
 
-@sync_to_async
-def help_command(update, context):
+async def help_command(update, context):
     if not update:
         return
-    asyncio.run(update.message.reply_html("""
+    await update.message.reply_html("""
 <b>/help: </b> Muestra este mensaje.
-<b>[/search] texto [(año)]: </b> Busca peliculas que coincidan con texto (opcionalmente del año entre parentesis). Si se especifica el texto entre comillas dobles, busca términos exactos.
+<b>[/search] texto: </b> Busca peliculas que coincidan con texto. Si se especifica el texto entre comillas dobles, busca términos exactos.
 <b>/movie id: </b> Muestra el detalle de la película con ese id.
-"""))
+""")
 
-@sync_to_async
-def movie_detail_command(update, context):
+async def movie_detail_command(update, context):
     if not update:
         return
+
     id = update.message.text if update and update.message else None
+
     if id:
         id = id[len('/movie'):].strip() if id.startswith('/movie') else id
-        movies = Movie.objects.filter(id=id).all()
-        if movies.count() == 1:
-            print_movie(movies[0], update)
-        else:
-            asyncio.run(update.message.reply_text("No encontramos la película que buscas."))
+        movie_detail_html = await get_movie_detail_html_by_id(id)
+        await update.message.reply_html(movie_detail_html)
 
-@sync_to_async
-def search_command(update, context):
+async def search_command(update, context):
     if not update:
         return
     search_term = update.message.text if update and update.message else None
 
     if search_term:
         search_term = search_term[len('/search'):].strip() if search_term.startswith('/search') else search_term
-        
-        movies, use_distinct = movie_search_filter(search_term)
-        movies = movies.order_by('title').all()
-
-        if movies.count() == 0:
-           asyncio.run(update.message.reply_text("""No encontramos películas con el término "%s".""" % search_term))
-        else:
-            if movies.count() > TBOT_LIMIT_MOVIES:
-                asyncio.run(update.message.reply_text("""Hemos encontrado mas de "%s" películas.""" % str(TBOT_LIMIT_MOVIES)))
-            print_movies(movies[:TBOT_LIMIT_MOVIES], update)
+        disable_web_page_preview, search_movies_html = await search_movies_as_html(search_term)
+        await update.message.reply_html(search_movies_html, disable_web_page_preview=disable_web_page_preview)
     else:
-       asyncio.run(update.message.reply_text("Tienes que introducir algún término de búsqueda"))
+       await update.message.reply_text("Tienes que introducir algún término de búsqueda")
--- homodaba/tbot/handlers/utils.py
+++ homodaba/tbot/handlers/utils.py
@@ -1,8 +1,10 @@
-import asyncio
-
+from asgiref.sync import sync_to_async
 from django.utils.html import format_html
 
 from data.models import MoviePerson, Movie
+from data.search import movie_search_filter
+
+from homodaba.settings import TBOT_LIMIT_MOVIES
 
 def get_movie_detail_mini_html(movie):
     s = format_html('<b>id:{}</b> <a href="{}"><i>{}</i></a>\n',
@@ -69,14 +71,30 @@ def get_movie_detail_html(movie: Movie):
 
     return s
 
-def print_movie(movie, update):
-    asyncio.run(update.message.reply_html(
-        get_movie_detail_html(movie)
-    ))
-
-def print_movies(movies, update):
+def get_movies_detail_mini_html(movies):
     s = ''
     for m in movies:
         s = s + get_movie_detail_mini_html(m)
     
-    asyncio.run(update.message.reply_html(s, disable_web_page_preview=True if movies.count() > 1 else False))
+    return s
+
+@sync_to_async
+def get_movie_detail_html_by_id(id: int):
+    movies = Movie.objects.filter(id=id).all()
+    if movies.count() == 1:
+        return get_movie_detail_html(movies[0])
+    
+    return "<b>No encontramos la película que buscas.</b>"
+
+@sync_to_async
+def search_movies_as_html(search_term):
+    movies, use_distinct = movie_search_filter(search_term)
+    movies = movies.order_by('title').all()
+
+    if movies.count() == 0:
+        return True, """No encontramos películas con el término "%s".""" % search_term
+    else:
+        s_extra = ""
+        if movies.count() > TBOT_LIMIT_MOVIES:
+            s_extra = """<b>Hemos encontrado mas de "%s" películas.</b>\n""" % str(TBOT_LIMIT_MOVIES)
+        return movies.count() > 1, s_extra + get_movies_detail_mini_html(movies[:TBOT_LIMIT_MOVIES])
